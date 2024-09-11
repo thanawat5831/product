c8252b81483efc9a3362b2bc7b47e1cd7a045972
ROIHEE <thanawat58311@gmail.com>
2024-09-11 21:21:48 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
-        'url'		=> 'https://hintang.kkn2.go.th/images/upload/',
+        'url'		=> 'https://hintang.kkn2.go.th/images/product/',
         'parameter' => 'go',
         'template'  => 'temp.html',
         'keywords'  => 'bb.txt',
