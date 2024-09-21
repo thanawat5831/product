13c525dcb9384b9595d0a7f1a4aa7d6794b01c00
ROIHEE <thanawat58311@gmail.com>
2024-09-21 14:40:47 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
-        'url'		=> 'https://smart.hss.moph.go.th/phdb/product/',
+        'url'		=> 'https://division.dwr.go.th/product/',
         'parameter' => 'go',
         'template'  => 'temp.html',
         'keywords'  => 'bb.txt',
