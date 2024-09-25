7303a4fce862a78269a9984a910b74b866ad0c28
ROIHEE <thanawat58311@gmail.com>
2024-09-25 21:28:28 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
-        'url'		=> 'https://division.dwr.go.th/product/',
+        'url'		=> 'https://www.lp-pao.go.th/Main60/images/banner/produc/',
         'parameter' => 'go',
         'template'  => 'temp.html',
         'keywords'  => 'bb.txt',
