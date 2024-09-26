eea249e1e32f35fb42fb4d42e403ecfdaba530e2
ROIHEE <thanawat58311@gmail.com>
2024-09-27 00:43:01 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 $config = array(
-        'url'		=> 'https://www.np.ac.th/np/stusys/produc/',
+        'url'		=> 'https://division.bangkok.go.th/dotat/product/',
         'parameter' => 'go',
         'template'  => 'temp.html',
         'keywords'  => 'bb.txt',
